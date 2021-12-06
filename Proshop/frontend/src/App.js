fab90dc1a5ffd794358b560a38cf2adb822adf2f
yasser.tarek <yasser.tarek@uflare.com>
2021-12-06 17:34:53 +0200

    First Section

--- Proshop/frontend/src/App.js
+++ Proshop/frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 const App = () =>  {
   return (
-    <>
-       <h1>Welcom To ProShop</h1>
-    </>
+    <Router>
+      <Header></Header>
+      <main className="py-3">
+          <Container>
+            <Routes>
+            <Route path = "/" element= {<HomeScreen/>} exact></Route>
+            <Route path = "/product/:id" element= {<ProductScreen/>} ></Route>
+            </Routes>
+         </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
   );
 }
 
